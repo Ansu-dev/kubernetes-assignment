abd1a1179cf019e46c006aa6398ee2383ff9da8e
Ansu-dev <ansu1007@naver.com>
2023-10-10 10:15:01 +0900

    ci/cd: ingress port 재설정

--- web-server/src/Router.tsx
+++ web-server/src/Router.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Default from "./pages/Default";
 import Home from "./pages/Home";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/web">
       <Routes>
-        <Route path="/" element={<Default />}></Route>
-        <Route path="/web" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
   );
